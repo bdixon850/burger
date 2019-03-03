7989ac17d0aaecab28c7b56e55c03b35480a96bb
Brandon Dixon <bdixon850@yahoo.com>
2019-03-02 21:07:55 -0500

    update

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Bacon Cheese Burger', false)
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Cheese Burger', true)
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Quarter Pounder', false)
